b0d815e8a32bab4d6e0a0a6b99507c66b5c940d2
lbuturovic <lbuturovic1@etf.unsa.ba>
2020-08-25 13:34:06 +0200

    dodano slanje maila

--- src/ba/unsa/etf/rpr/projekat/SendEmailsController.java
+++ src/ba/unsa/etf/rpr/projekat/SendEmailsController.java
@@ -6,6 +6,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -83,6 +90,39 @@ public class SendEmailsController extends Controler {
     }
 
     public void sendEmailsAction(ActionEvent actionEvent) {
-
+        final String email = user.getEmail();
+        final String pass = user.getPassword();
+        //Setting up configurations for the email connection to the Google SMTP server using TLS
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(email, pass);
+            }
+        });
+        try {
+            //Creating a Message object to set the email content
+            MimeMessage msg = new MimeMessage(session);
+            //Storing the comma seperated values to email addresses
+            String to = addresses;
+            //Parsing the String with defualt delimiter as a comma by marking the boolean as true and storing the email   addresses in an array of InternetAddress objects
+            InternetAddress[] address = InternetAddress.parse(to, true);
+            //Setting the recepients from the address variable
+            msg.setRecipients(Message.RecipientType.TO, address);
+            String timeStamp = new SimpleDateFormat("yyyymmdd_hh-mm-ss").format(new Date());
+            msg.setSubject(areaSubject.getText()+": " + timeStamp);
+            msg.setSentDate(new Date());
+            msg.setText(areaMail.getText());
+            msg.setHeader("XPriority", "1");
+            Transport.send(msg);
+            message.setText("Mail has been sent successfully");
+        } catch (MessagingException mex) {
+            message.setText("Unable to send an email" + mex);
+        }
     }
+
 }
